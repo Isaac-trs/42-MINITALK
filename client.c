734f332fc54178e714cdaabd5a9eaf2e9f1b4608
istripol <istripol@student.42.fr>
2024-10-16 03:39:14 +0200

    BEEN WORKING SINCE 1DAY, FCK PRINTF... Clean and delete forbidden libs

--- client.c
+++ client.c
@@ -14,24 +14,32 @@
 
 int		serv_pid;
 
-void	send_char(pid_t pid, char c)
+void	send_char(pid_t pid, unsigned char c)
 {
 	int	bit_index;
+	static int j = 1;
 	
 	bit_index = 7;
 	while (bit_index >= 0)
 	{
 		if ((1 << bit_index) & c)
+		//if ((c >> bit_index ) & 1)
+		{
 			kill(pid, SIGUSR1);
+			printf("%i) signal sent SIGUSR1\n", j);
+		}	
 		else
+		{	
 			kill(pid, SIGUSR2);
+			printf("%i) signal sent SIGUSR2\n", j);
+		}
 		bit_index--;
-		usleep(100);
+		usleep(42);
+		j++;
 	}
-	
 }
 
-void	send_message(pid_t pid, char *str)
+void	send_message(pid_t pid, unsigned char *str)
 {
 	while(*str)		
 	{
@@ -44,16 +52,19 @@ void	send_message(pid_t pid, char *str)
 int	main(int ac, char **av)
 {
 	unsigned char tmp = 0;
-	printf("a = 0\n");
-	tmp = 1 << 7 | mtp; // 128 = 1000 000
-				//  0   0000 000 	OR = 1000 0000	AND = 0000 0000 	XOR = 1000 0000
-			1 << 6 | tmp; // 64	= 0100 0000 & 1000 0000		tmp = 0100 0000
-			1 << 5 | tmp; // 32	= 0010 0000 & 0100 0000		tmp = 0010 0000
-	if (ac != 3)
+
+
+
+if (ac != 3)
 		return(printf("Wrong number of args !\n"));
 	serv_pid = atoi(av[1]);
 	if (!serv_pid || kill(serv_pid, 0) == -1)
 		return(printf("Wrong PID!\n"));
+	//send_message(serv_pid, av[2]);
+	int i = 0;
+	int j = 1;
+
+	printf("message: |%s|\n", av[2]);
 	send_message(serv_pid, av[2]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -10,20 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "minitalk.h"
+
+static void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	handler(int signum)
 {
-	static int	bits_received = 0;
-	char c = 0;
+	static int	bit_index;
+	static unsigned char c;
 
-	while (bits_received < 8)
+	//First bit to be transmitted
+	c |= (signum == SIGUSR1);
+	/*if (signum == SIGUSR1)
+		write(1, "RECEIVED SIGUSR1\n", 17);
+	if (signum == SIGUSR2)
+		write(1, "RECEIVED SIGUSR2\n", 17);
+	*/
+	bit_index++;
+
+	if (bit_index == 8)
 	{
-		if (signum == SIGUSR1)		
-			c = c >> bits_received & 1;
+		if (c == 0)
+			write(1, "\n", 1);
+		else
+		{
+			ft_putchar(c);
+		//	write(1, "\n", 1);
+		}
+		bit_index = 0;
+		c = 0;
 	}
+	else
+		c <<=1;
 }
+
 void	handler_sigusr1(int signum)
 {
 	write(1, "Received SIGUSR1\n", 18);
@@ -38,23 +61,26 @@ void	handler_sigusr2(int signum)
 
 int main()
 {
-	struct sigaction	sa_sigusr1;
-	struct sigaction	sa_sigusr2;
+//	struct sigaction	sa_sigusr1;
+//	struct sigaction	sa_sigusr2;
+	struct sigaction	sa;
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+		// sa_sigusr1.sa_handler = handler_sigusr1;
+		// sigemptyset(&sa_sigusr1.sa_mask);
+		// sa_sigusr1.sa_flags = 0;
 
-	sa_sigusr1.sa_handler = handler_sigusr1;
-	sigemptyset(&sa_sigusr1.sa_mask);
-	sa_sigusr1.sa_flags = 0;
+		//	sa_sigusr2.sa_handler = handler_sigusr2;
+		//	sigemptyset(&sa_sigusr2.sa_mask);
+		//	sa_sigusr2.sa_flags = 0;
 
-	sa_sigusr2.sa_handler = handler_sigusr2;
-	sigemptyset(&sa_sigusr2.sa_mask);
-	sa_sigusr2.sa_flags = 0;
-
-	int	elPid = getpid();    
-    printf("server PID is  %d\n", elPid);
+	int elPid = getpid();
+	printf("server PID is  %d\n", elPid);
 	printf("Now waiting for messages...\n\n");
 	
-	sigaction(SIGUSR1, &sa_sigusr1, NULL);
-	sigaction(SIGUSR2, &sa_sigusr2, NULL);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
         
     while (1)
 	{
